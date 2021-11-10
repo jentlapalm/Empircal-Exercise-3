b04462e95b374f928610701d6f1d6bd26c681a66
jentlapalm <81242269+jentlapalm@users.noreply.github.com>
2021-11-10 14:15:50 -0500

    Add files via upload

--- EE#3.R
+++ EE#3.R
@@ -10,14 +10,17 @@ library(stringr)
 library(lubridate)
 library(ggplot2)
 library(ggthemes)
+library(cowplot)
+
+install.packages("cowplot")
 
 install.packages("PearsonDS")
 library("PearsonDS")
 
 # home wd
-setwd("C:\\Users\\Jent\\Documents\\College\\Emory\\Social Network Analytics\\EE#3")
+# setwd("C:\\Users\\Jent\\Documents\\College\\Emory\\Social Network Analytics\\EE#3")
 # school wd
-#setwd("C:\\Users\\jentl\\Documents\\Emory\\Fall 2021\\Social Network Analytics\\EE#3")
+setwd("C:\\Users\\jentl\\Documents\\Emory\\Fall 2021\\Social Network Analytics\\EE#3")
 
 "Loading Data"
 
@@ -64,18 +67,66 @@ rain[,SPI:=(qpear3-mean(qpear3))/sd(qpear3),by=.(district,trunc(time))]
 # dropping rows with NA SPI values
 rain <- rain[!is.na(SPI),]
 
+# create election year flag
+
+# year column for joining
+rain[,year:=trunc(time)]
+
+# joining tables by year, creating election year flag
+rain[election, on='year',elect_year:=year]
+
+# replace elect_year NA with 0
+set(rain,which(is.na(rain[[6]])),6,0)
+
+# creating elect_year_group
+
+unique_years <- unique(rain$elect_year)
+
+for (i in 1:(length(unique_years)-1)){
+  rain[unique_years[i]<rain$year & rain$year <= unique_years[i+1],elect_group:=i]
+}
+
+# calculate number of parties founded each election-interval
+
+# get list of parties in district in each election, all previous elections
+
+# call setdiff on these lists to find, count the number of different parties
+
+new_party <- data.table(district=character(), year=numeric(), new_parties=numeric())
+
+# need a year zero b/c parties are 'founded' in the first election
+elect_years <- c(0,unique(election$year))
+
+for (i in 1:(length(elect_years-1))){
+  # list of new parties 
+  temp1 <- setdiff(election[year==elect_years[i+1],party_name,by=district],
+  election[year<=elect_years[i],party_name,by=district])
+  # number of new parties by district
+  temp1[,new_parties:=length(unique(party_name)),by=district]
+  # creating new party table
+  temp2 <- election[year==unique(election$year)[i]][temp1, on='district', new_parties:=new_parties]
+  new_party <- rbind(new_party,temp2[,.(district,year,new_parties)])
+}
+
+# joining new_party to rain by district and year
+rain[new_party, new_parties:=i.new_parties, on=c(year='year',district='district')]
+
 "1(A).
 Create a scatter plot, showing relationship between 
 average SPI in a district across each year from the prior election or first election up to the current election, and 
 the number of political parties that are founded in a district. 
-Consider the party to be "founded" if it has not competed in an election in this district prior to this election."
+Consider the party to be 'founded' if it has not competed in an election in this district prior to this election. X axis is avg SPI for a district in an election-interval, y axis is num parties founded"
 
-# nail down election year intervals in election dt
+"clarify: am I graphing per district or aggregate?"
+# calculate average SPI by district by year
+rain[,avg_SPI:=mean(SPI),by=.(district,year)]
 
-# calculate number of parties founded by election-interval
+# setting NA new parties to zero
+set(rain,which(is.na(rain[[12]])),12,0)
 
-# join with average SPI by district for each election-year interval
+# table for SPI 
+rain_plot <- unique(rain[,.(avg_SPI,new_parties),by=.(district,year)])
 
-# create a scatter plot of SPI by number of parties for a district
+# using ggplot, adding geom_smooth trend line
+ggplot(rain_plot, aes(avg_SPI,new_parties))+geom_point()+xlab("Average SPI")+ylab("Number of New Parties")+ggtitle("Number of New Indian Political Parties vs. SPI by District each Year ")+theme_classic()
 
-# create for loop to apply to all districts
